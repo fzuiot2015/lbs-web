97fa7e21738729f2eddbaef9ba8b0638eadc074e
zengjinrong <jinrong.zeng@outlook.com>
2018-09-13 21:37:19 +0800

    feat: 引入ElementUI

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false;
 Object.defineProperty(Vue.prototype, '$axios', {value: axios});
 
