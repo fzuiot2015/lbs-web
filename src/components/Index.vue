f8980c654ccfefeaa6cff641a2052d000bced7c2
zengjinrong <jinrong.zeng@outlook.com>
2018-09-15 20:20:57 +0800

    fix: 调整侧边栏宽度

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,17 +1,23 @@
 <template>
   <el-container>
+
     <side-bar></side-bar>
-    <router-view></router-view>
+
+      <router-view></router-view>
+
   </el-container>
 </template>
 
 <script>
 
   import SideBar from "./SideBar";
+  import UserContent from "./user/UserContent";
+  import CarContent from "./car/CarContent";
+  import AccidentContent from "./accident/AccidentContent";
 
   export default {
     name: "Index",
-    components: {SideBar},
+    components: {AccidentContent, CarContent, UserContent, SideBar},
   }
 </script>
 
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,10 @@
 <template>
-  <el-aside>
-    <el-menu  :default-active="$route.path" class="el-menu-vertical-demo" router>
+  <el-aside width="150px">
+    <el-menu :default-active="$route.path" router class="el-menu-vertical-demo">
+
+      <!--background-color="#545c64" text-color="#fff" active-text-color="#ffd04b"-->
+
+
       <el-menu-item index="/user">
         <i class="el-icon-document"></i>
         <span slot="title">用户信息</span>
